4a063f6ff6b36e6566f045c7f9d9479caf5a461e
Mauro <mauro_leao@hotmail.com>
2026-04-07 11:00:57 -0300

    criando o cabeçalho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,31 @@
-import { Header as HeaderBar } from './style'
+import { Header as HeaderBar, Links, LinkItem, LinkCart } from './style'
 
 import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
 
 const Header = () => (
   <HeaderBar>
-    <img src={logo} alt="EPLAY" />
+    <div>
+      <img src={logo} alt="EPLAY" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#home">Categorias</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#about">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#contact">Promoções</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+
+    <LinkCart>
+      0 - produto(s)
+      <img src={carrinho} alt="Carrinho" />
+    </LinkCart>
   </HeaderBar>
 )
 
